3fb4812c4094178ae7c5fc980163aecaf39a4479
Julio Angel Molina Nunez <donjulio0588@gmail.com>
2023-03-02 17:38:14 +0100

    Commit inicial

---
+++ suma.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.suma = void 0;
+var suma = function (a, b) {
+    return a + b;
+};
+exports.suma = suma;
---
+++ suma.ts
@@ -0,0 +1,5 @@
+const suma = (a: number, b: number): number =>{
+    return a + b
+}
+
+export { suma }
